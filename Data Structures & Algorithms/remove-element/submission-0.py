ae98b2e2c38f41dd3a999d6e2cfb15929d9e03af
Ernest Au <75875992+ErnestAu@users.noreply.github.com>
2026-05-13 10:12:32 +0800

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.py
@@ -0,0 +1,19 @@
+# remove all val
+# return size nums as k
+
+# 2 pointer
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+        right = len(nums) - 1
+        
+        while k <= right:
+            if nums[k] == val:
+                nums[k] = nums[right]
+                right -= 1
+            else:
+                k += 1
+        return k 
+
+       
