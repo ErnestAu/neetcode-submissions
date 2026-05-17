d50cc82a0e25c024d826cabdbc5b67fdc007b16c
Ernest Au <75875992+ErnestAu@users.noreply.github.com>
2026-05-17 11:41:29 +0800

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.py
@@ -0,0 +1,33 @@
+# piles[i] = number of bananas in that pile
+# h = no of horus to eat all the bananas
+
+# k = banana eat / hr
+
+# if piles[i] < k
+
+# whats the minimum rate to finish all bananas within h hours
+
+
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles)
+        while l<r:
+            k = (l + r) // 2
+            if self.eatBananasTime(piles, k) <= h:
+                r = k 
+            else:
+                l = k + 1
+        return l
+
+    def eatBananasTime(self,piles, k):
+        h = 0
+        for pile in piles:
+            h += math.ceil(pile / k)
+            # if pile % k != 0:
+            #     eatTime += 1
+            # h += eatTime
+        return h
+
+
