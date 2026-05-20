8997151c883f48f9f8e0d62e9a938acac858172f
Ernest Au <75875992+ErnestAu@users.noreply.github.com>
2026-05-20 15:50:16 +0800

    Add: implement-stack-using-queues - submission-4

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-4.py
@@ -0,0 +1,56 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = None
+
+    def push(self, x: int) -> None:
+        self.q = deque([x, self.q])
+
+    def pop(self) -> int:
+        top = self.q.popleft()
+        self.q = self.q.popleft()
+        return top
+
+    def top(self) -> int:
+        return self.q[0]
+
+    def empty(self) -> bool:
+        return not self.q
+
+# class MyStack:
+
+#     def __init__(self):
+#         self.q = deque()
+#         self.r = deque()
+        
+#     def push(self, x: int) -> None:
+#         self.q.append(x)
+
+#     def pop(self) -> int:
+#         self.r = deque()
+#         for i in range(len(self.q)-1):
+#             self.r.append(self.q.popleft())
+            
+#         final = self.q[0]
+#         self.q = self.r
+#         return final
+
+#     def top(self) -> int:
+#         self.r = deque()
+#         for i in range(len(self.q)):
+#             final = self.q[0]
+#             self.r.append(self.q.popleft())
+#         self.q = self.r
+#         return final
+
+
+#     def empty(self) -> bool:
+#         return not self.q
+
+
+# # Your MyStack object will be instantiated and called as such:
+# # obj = MyStack()
+# # obj.push(x)
+# # param_2 = obj.pop()
+# # param_3 = obj.top()
+# # param_4 = obj.empty()
