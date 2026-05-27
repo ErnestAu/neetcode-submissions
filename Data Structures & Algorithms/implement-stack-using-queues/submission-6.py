ae31be1160a1cd20ba5bf40057b2ff2c1d987e45
Ernest Au <75875992+ErnestAu@users.noreply.github.com>
2026-05-27 10:58:10 +0800

    Add: implement-stack-using-queues - submission-6

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-6.py
@@ -0,0 +1,44 @@
+# push to back (append())
+# peep from front (arr[0])
+# size (len())
+# is empty (isEmpty)
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+        self.r = deque()
+        self.size = 0        
+    def push(self, x: int) -> None:
+        self.q.append(x)
+        self.size += 1
+
+    def pop(self) -> int:
+        for _ in range(self.size-1):
+            self.r.append(self.q.popleft())
+
+        val = self.q.popleft()
+        self.q = self.r
+        self.size -= 1
+        return val
+
+    def top(self) -> int:
+        for _ in range(self.size-1):
+            self.r.append(self.q.popleft())
+
+        val = self.q[0]
+        self.r.append(self.q.popleft())
+        self.q = self.r
+        return val
+
+
+    def empty(self) -> bool:
+        return self.size == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
