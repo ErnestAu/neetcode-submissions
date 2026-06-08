857ba7801c7dfb4b5a27c435e898e3b7896187bf
Ernest Au <75875992+ErnestAu@users.noreply.github.com>
2026-06-08 08:40:04 +0800

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,62 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result = set()
+
+        for i in range(len(nums)):
+            l = i + 1
+            r = len(nums)-1
+            diff = -nums[i]
+            while l < r:
+                if nums[l] + nums[r] > diff:
+                    r -= 1
+                elif nums[l] + nums[r] < diff:
+                    l += 1
+                else:
+                    result.add(tuple([nums[i], nums[l], nums[r]]))
+                    l += 1
+        
+        return [list(i) for i in result]
+
+        # for i in range(len(nums)):
+        #     count[nums[i]] -= 1
+        #     if i and nums[i] == nums[i-1]:
+        #         continue
+
+
+
+
+
+
+
+
+
+
+        # res = []
+        # for i in range(len(nums)):
+        #     count[nums[i]] -= 1
+        #     if i and nums[i] == nums[i - 1]:
+        #         continue
+
+        #     for j in range(i + 1, len(nums)):
+        #         count[nums[j]] -= 1
+        #         if j - 1 > i and nums[j] == nums[j - 1]:
+        #             continue
+        #         target = -(nums[i] + nums[j])
+        #         if count[target] > 0:
+        #             res.append([nums[i], nums[j], target])
+
+        #     for j in range(i + 1, len(nums)):
+        #         count[nums[j]] += 1
+        # return res
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         res = set()
+#         nums.sort()
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 for k in range(j + 1, len(nums)):
+#                     if nums[i] + nums[j] + nums[k] == 0:
+#                         tmp = [nums[i], nums[j], nums[k]]
+#                         res.add(tuple(tmp))
+#         return [list(i) for i in res]
